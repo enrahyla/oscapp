90e4553436e0ff47a11e86d6724eab971f386f22
enrahyla <A.D.Hyland@2009.ljmu.ac.uk>
2013-02-05 14:47:13 +0000

    update
    index page working and about page working

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -9,4 +9,16 @@ class WelcomeController < ApplicationController
   def contact_us
   end
 
+  def faq
+  end
+
+  def social
+  end
+
+  def racing_team
+  end
+
+  def lessons
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,12 @@ Oscapp::Application.routes.draw do
   
   root :to => 'welcome#index'
 
+  get 'welcome/index'
+  get 'welcome/aboutus'
+  get 'welcome/contactus'
+  get 'welcome/faq'
+  get 'welcome/social'
+  get 'welcome/racingteam'
+  get 'welcome/lessons'
+  
 end
