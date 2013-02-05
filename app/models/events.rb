074af9fca60241b95c2d273860ad41d1ff20905b
enrahyla <A.D.Hyland@2009.ljmu.ac.uk>
2013-02-05 19:43:32 +0000

    pdate
    not working code

--- app/models/events.rb
+++
@@ -1,3 +0,0 @@
-class Events < ActiveRecord::Base
-  # attr_accessible :title, :body
-end
--- db/migrate/20130205192256_create_events.rb
+++ db/migrate/20130205192256_create_events.rb
@@ -1,8 +1,9 @@
 class CreateEvents < ActiveRecord::Migration
   def change
     create_table :events do |t|
-
-      t.timestamps
+    	t.string :title
+    	t.text :body
+    	t.timestamps
     end
   end
 end
